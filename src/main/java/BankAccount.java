7b70cecc1169b92bdfb0ee8d7b1e6a1146fe2973
Michelle <mcheng24@illinois.edu>
2018-02-20 16:40:34 -0600

    yoyoyoing

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -13,26 +13,85 @@ public class BankAccount {
      * You may want to use this to distinguish between different kinds of accounts.
      */
     public enum BankAccountType {
+        /***/
         CHECKINGS,
+        /***/
         SAVINGS,
+        /***/
         STUDENT,
+        /***/
         WORKPLACE
     }
 
+    /***/
     private int accountNumber;
+    /***/
     public BankAccountType accountType;
+    /***/
     private double accountBalance;
+    /***/
     private String ownerName;
+    /***/
     public double interestRate;
+    /***/
     private double interestEarned;
 
+    /**
+     *
+     * @param name ownerName
+     * @param accountCategory accountType
+     */
     public BankAccount(final String name, final BankAccountType accountCategory) {
         /*
          * Implement this function
          */
+        ownerName = name;
+        accountType = accountCategory;
     }
 
     /*
      * Implement getters and setters as appropriate for private variables.
      */
+
+    /**
+     * @return account number
+     */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * @return accountType
+     */
+    public BankAccountType getAccountType() {
+        return accountType;
+    }
+
+    /**
+     * @return account balance
+     */
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    /**
+     * @return owner name
+     */
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    /**
+     * @return interestRate
+     */
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    /**
+     * @return interest earned
+     */
+    public double getInterestEarned() {
+        return interestEarned;
+    }
 }
